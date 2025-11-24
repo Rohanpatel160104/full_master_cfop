c4af0445f7323b85ea87af7c11c70fb39fa61d3c
Rohanpatel160104 <tigi.bde@gmail.com>
2025-11-24 18:16:14 +0530

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/master_cfop/',
+      base: '/full_master_cfop/',
       server: {
         port: 3000,
         host: '0.0.0.0',
